81af77a57ea503b37c7acc295ab0b5ffe8a70c20
kevinaaronson <kevinaaronson@gmail.com>
2020-11-03 15:55:00 -0600

    profile edit
    Had some database call errors

--- Project/profile.php
+++ Project/profile.php
@@ -8,8 +8,8 @@ if (!isset($_SESSION['loggedin'])) {
 }
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_PASS = 'root';
+$DATABASE_NAME = 'restaurant_app';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
